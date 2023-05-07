92c49c3925b5a0221df0f8d1519391dae96b7914
JoseDaddy42 <eliasballadares2004@gmail.com>
2023-05-06 21:56:53 -0500

    wasa

--- resources/scripts/section detector.js
+++ resources/scripts/section detector.js
@@ -1,20 +1,20 @@
-let section = document.querySelectorAll("section");
-let navLinks = document.querySelectorAll("header div nav a");
+let section = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header div nav a');
+
 window.onscroll = () => {
-  section.forEach(sec =>{
 
-    let top = window.scrollY;
-    let offset = sec.offsetTop;
-    let height = sec.offsetHeight;
-    let id = sec.getAttribute('id')
+        section.forEach(sec =>{
 
-    if(top => offset && top < offset + height){
-        navLinks.forEach(links =>{
-            links.classList.remove('active');
-            document.querySelector('header nav a [href*=' + id + ']').classList.add
-            ('active');
-        }
-            )
-    }
-}
+        let top = window.scrollY;
+        let offset = sec.offsetTop;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id')
+
+        if(top => offset && top < offset + height){
+            navLinks.forEach(links =>{
+                links.classList.remove('active');
+                document.querySelector('header nav a [href*=' + id + ']').classList.add('active');
+            });
+        };
+    });
 };
